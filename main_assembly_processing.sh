feab3c8823a5575f0384d74da5d2ca61febd12f3
Kalyan Sahu <ksahu2@login.crane.hcc.unl.edu>
2021-12-16 11:51:59 -0600

    changed directory

--- main_assembly_processing.sh
+++ main_assembly_processing.sh
@@ -6,9 +6,10 @@
 #SBATCH --output=assembly.%J.out
 #SBATCH --error=assembly%J.err
 
+
 A=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533/SRR9205533_1.fastq.filtered_1.fastq
 B=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533/SRR9205533_2.fastq.filtered_2.fastq
 C=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533/no_filtered
 t=8
 D=0
-./assembly_binning.sh $A $B $C $t $D
+/home/yaolab/ksahu2/.ssh/metaIMP/assembly_binning.sh $A $B $C $t $D
