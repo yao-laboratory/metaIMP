1d18ba05a240315a8ddb8674f7a4dc28fe5c9003
Kalyan Sahu <ksahu2@login.crane.hcc.unl.edu>
2021-12-15 23:54:10 -0600

    ignore this change

--- main_assembly_processing.sh
+++ main_assembly_processing.sh
@@ -6,10 +6,9 @@
 #SBATCH --output=assembly.%J.out
 #SBATCH --error=assembly%J.err
 
-
-A=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205534/SRR9205534_1.fastq
-B=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205534/SRR9205534_2.fastq
-C=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205534
+A=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533/SRR9205533_1.fastq.filtered_1.fastq
+B=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533/SRR9205533_2.fastq.filtered_2.fastq
+C=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533
 t=8
 
 ./main_assembly_processing.sh $A $B $C $t
