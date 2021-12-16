010c4cf8bb7b05f979f8321fa28512c9875ce4de
Kalyan Sahu <ksahu2@login.crane.hcc.unl.edu>
2021-12-16 10:28:41 -0600

    assembly bug fixed

--- main_assembly_processing.sh
+++ main_assembly_processing.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=assembly
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=24gb
+#SBATCH --mem=48gb
 #SBATCH --output=assembly.%J.out
 #SBATCH --error=assembly%J.err
 
@@ -11,4 +11,4 @@ B=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533/SRR92
 C=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205533
 t=8
 
-./main_assembly_processing.sh $A $B $C $t
+./assembly_binning.sh $A $B $C $t
