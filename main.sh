a0be8e101860776816ad3069f20640826625e136
kaysahu <14kalyansahu@gmail.com>
2021-12-21 01:00:20 -0600

    final changes for the night before submitting job

--- main.sh
+++ main.sh
@@ -39,7 +39,13 @@ echo " no need to load python module. exists in env "
 #modules for assembly_snp_calling.sh:
 module load instrain/1.3
 export PYTHONNOUSERSITE=1
-export USER_ENV_NAME==metaimp_env 
+
+#env_var_func() {
+#	echo "metaimp_env"
+#}
+#this was an attempt to use function. probably delete this. use simple names
+
+export USER_ENV_NAME=metaimp_env
 
 fastq1=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205535/SRR9205535_1.fastq
 fastq2=/work/yaolab/shared/2021_milk_2017_metagenome/MIT_DATA/sample_SRR9205535/SRR9205535_2.fastq
