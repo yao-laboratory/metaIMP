5d4c2230e2037df289de8d2a48765821f48b82c4
kaysahu <14kalyansahu@gmail.com>
2022-01-27 09:30:49 -0600

    OSG example job added

---
+++ example/example_osg_wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+EXPECTED_ARGS=5
+
+if [ $# -ne $EXPECTED_ARGS ]; then
+	 echo "Usage: metaimp testing"
+	 exit 1
+else
+	 source activate metaimp_env3
+	 /home/yaolab/ksahu2/.ssh/metaIMP/main_assembly_processing.sh $1 $2 $3 $4 $5
+	 conda deactivate
+fi
+	
