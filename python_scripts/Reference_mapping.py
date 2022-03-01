9c274a0e98f69cb9266b29369993099f37e69059
kaysahu <14kalyansahu@gmail.com>
2022-03-01 11:30:44 -0600

    modified func for one data

--- python_scripts/Reference_mapping.py
+++ python_scripts/Reference_mapping.py
@@ -54,7 +54,62 @@ def reference_mapping(patric_path, midas_snps_path, output_path): #for looping
         output_fullpath_filename = join(output_path, species+"_patric_midassnps.csv")
         reference_mapping_for_one_data(patric_fullpath, snp_fullpath, output_fullpath_filename)
 
+
 def reference_mapping_for_one_data(patric_fullpath_filename,midas_snp_fullpath_filename, output_fullpath_filename):
+    patric_op = pd.read_csv(patric_fullpath_filename, sep="\t")
+    midas_snps = pd.read_csv(midas_snp_fullpath_filename, sep="\t",compression ='gzip')
+
+    if patric_op.shape[0] !=0:
+        print(patric_fullpath_filename," is not empty, we continue processing")
+        patric_op.rename(columns={'feature.accession':'ref_id'},inplace='True')
+        mut_count= midas_snps[['count_a','count_c','count_g','count_t']].max(axis=1)
+        count_equals = mut_count.eq(midas_snps['depth'])
+        midas_snps['count_equals']= count_equals
+
+        mutations = midas_snps['count_equals'].values == 0
+        mutation_list = midas_snps[mutations]
+
+        mutation_list=mutation_list.sort_values(by='ref_id', ascending=True)
+
+        df_reference_final = pd.DataFrame([])
+        patric_op_sub=pd.DataFrame([])
+
+        pre_ref_id=""
+
+        count=0
+        for j in mutation_list.index:
+            if count%100==0:
+                print(count)
+            count=count+1
+
+            current_ref_id=mutation_list.loc[j]['ref_id']
+            mutation_pos=mutation_list.loc[j]['ref_pos']
+            a=mutation_list.loc[[j], :]
+
+
+            if current_ref_id!=pre_ref_id:
+                patric_op_sub=patric_op.loc[patric_op['ref_id']==current_ref_id]
+                pre_ref_id=current_ref_id
+
+            for i in patric_op_sub.index:
+                start_pos=patric_op.loc[i]['feature.start']
+                end_pos=patric_op.loc[i]['feature.end']
+                if mutation_pos>=start_pos and mutation_pos<=end_pos:
+                    b=patric_op.loc[[i], :]
+                    newrow=pd.merge(a,b,on='ref_id',how='inner')
+                    df_reference_final =pd.concat([df_reference_final,newrow],axis=0,ignore_index=True)
+        df_assembly_final.to_csv(output_fullpath_filename, sep=",",index=None)
+        print('FINISH!!!!')
+
+    else:
+        print(patric_fullpath,"this patric file is empty. skipping this file")
+
+
+
+
+
+
+def reference_mapping_for_one_data_old(patric_fullpath_filename,midas_snp_fullpath_filename, output_fullpath_filename):
     patric_op = pd.read_csv(patric_fullpath_filename, sep="\t")
     midas_snps = pd.read_csv(midas_snp_fullpath_filename, sep="\t",compression ='gzip')
     if patric_op.shape[0] != 0:
