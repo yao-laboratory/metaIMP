d09441674c945b33daa0db2b05fead5cbccc5205
kaysahu <14kalyansahu@gmail.com>
2022-03-06 22:33:42 -0600

    data_dir removed.testing

--- assembly_contig_annotation.sh
+++ assembly_contig_annotation.sh
@@ -12,7 +12,7 @@ if [ ! -d "$eggnog_output_folder" ] ; then
         mkdir $eggnog_output_folder
 fi
 eggnog_output=$eggnog_output_folder/eggnog_results
-emapper.py -i $mergedfile -o $eggnog_output --cpu 0 --data_dir $EGGNOG_DIAMOND_DATABASE
+emapper.py -i $mergedfile -o $eggnog_output --cpu 0 #--data_dir $EGGNOG_DIAMOND_DATABASE
 
 echo 'Assembly_contig annotation is complete. Check' $eggnog_output 
 
