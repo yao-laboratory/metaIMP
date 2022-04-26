59ae496bcddd9b6fcd296e20e6debb7cbf3b3144
kaysahu <14kalyansahu@gmail.com>
2022-04-26 07:10:30 -0500

    remove comments after testing

--- ref_protein_annotation.sh
+++ ref_protein_annotation.sh
@@ -15,15 +15,15 @@ fi
 DIR="${BASH_SOURCE[0]}"
 DIR="$(dirname "$DIR")"
 
-python $DIR/python_scripts/Reference_mapping.py ref_map -pf $patric_folder -sf $snps_folder -o $output_folder_final
+#python $DIR/python_scripts/Reference_mapping.py ref_map -pf $patric_folder -sf $snps_folder -o $output_folder_final
 echo 'completed reference_mapping. Good luck!!!'
 
 echo 'creating VCF reference output file'
-python $DIR/python_scripts/VCF_converter_reference.py vcf_map_reference -i $output_folder -o $output_folder_final
+#python $DIR/python_scripts/VCF_converter_reference.py vcf_map_reference -i $output_folder -o $output_folder_final
 echo 'VCF file created for reference process'
 
 echo 'calculating Amino acid mutations'
-python $DIR/python_scripts/Reference_AA_mapping.py aa_map_reference -i $output_folder -o $output_folder_final
+#python $DIR/python_scripts/Reference_AA_mapping.py aa_map_reference -i $output_folder -o $output_folder_final
 #echo 'aa_map_reference -i $output_folder -o $output_folder'
 echo 'AA mutations calcuated'
 
@@ -34,8 +34,9 @@ echo "species summary is done at $(date)"
 
 echo "starting ec and go map"
 
-python $DIR/python_scripts/Reference_EC_mapping.py ec_map -i $output_folder -o $output_folder_final
+python $DIR/python_scripts/Reference_EC_mapping.py ec_map -i $output_folder_final -o $output_folder_final
+echo "python $DIR/python_scripts/Reference_EC_mapping.py ec_map -i $output_folder_final -o $output_folder_final"
 
-python $DIR/python_scripts/Reference_GO_mapping.py go_map -i $output_folder -o $output_folder_final
+python $DIR/python_scripts/Reference_GO_mapping.py go_map -i $output_folder_final -o $output_folder_final
 
 echo "finished ec go map $(date)"
