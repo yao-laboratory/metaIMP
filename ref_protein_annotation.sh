fcd264f022ccb5990e7d347e6a258298157a11ef
kaysahu <14kalyansahu@gmail.com>
2022-04-28 12:22:13 -0500

    input folder to vcf+aa_mapping update

--- ref_protein_annotation.sh
+++ ref_protein_annotation.sh
@@ -15,15 +15,15 @@ fi
 DIR="${BASH_SOURCE[0]}"
 DIR="$(dirname "$DIR")"
 
-python $DIR/python_scripts/Reference_mapping.py ref_map -pf $patric_folder -sf $snps_folder -o $output_folder_final
+#python $DIR/python_scripts/Reference_mapping.py ref_map -pf $patric_folder -sf $snps_folder -o $output_folder_final
 echo 'completed reference_mapping. Good luck!!!'
 
 echo 'creating VCF reference output file'
-python $DIR/python_scripts/VCF_converter_reference.py vcf_map_reference -i $output_folder -o $output_folder_final
+python $DIR/python_scripts/VCF_converter_reference.py vcf_map_reference -i $output_folder_final -o $output_folder_final
 echo 'VCF file created for reference process'
 
 echo 'calculating Amino acid mutations'
-python $DIR/python_scripts/Reference_AA_mapping.py aa_map_reference -i $output_folder -o $output_folder_final
+python $DIR/python_scripts/Reference_AA_mapping.py aa_map_reference -i $output_folder_final -o $output_folder_final
 echo 'aa_map_reference -i $output_folder -o $output_folder'
 echo 'AA mutations calcuated'
 
