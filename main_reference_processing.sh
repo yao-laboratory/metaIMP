29bcf8f84ba435565324641133c89c2c9966bbe0
kaysahu <14kalyansahu@gmail.com>
2022-09-01 12:15:25 -0500

    main ref and ref_protein_annot revert back to original

--- main_reference_processing.sh
+++ main_reference_processing.sh
@@ -7,7 +7,6 @@ sampleID=$3
 database_folder=$4
 output_folder=$5
 min_thread=$6
-midas_tax_db=$7
 
 if [ ! -d "$output_folder" ] ; then
         mkdir $output_folder
@@ -101,24 +100,10 @@ else
                 echo "$log_protein_annotation exists. Skipping annotations @ $(date) "
         else
                 echo "$log_protein_annotation does not exist. Starting SNP_annotation at $(date)"
-		$DIR/ref_protein_annotation.sh $output_folder/MIDAS/genes/output $output_folder/MIDAS/genes/output $output_folder/MIDAS/snps/output $output_folder/ $midas_tax_db
+		$DIR/ref_protein_annotation.sh $output_folder/MIDAS/genes/output $output_folder/MIDAS/genes/output $output_folder/MIDAS/snps/output $output_folder/ 
 		echo "Finished protein_annotaion at $(date)."
 		touch $log_protein_annotation
 	fi
-
-	log_protein_annotation_post=$log_folder/protein_annotation_post.log
-
-	if [ -f "$log_protein_annotation_post" ] ; then
-		echo "$log_protein_annotation_post exists. No log creation @ $(date)"
-	else
-		echo "$log_protein_annotation_post does not exists. Log creation @ $(date)"
-		echo "#### PROTEIN_ANNOTATION_LOG CREATED AT #### $(date)."
-		touch $log_protein_annotation
-	fi
-		
-
-	
-
 
 	echo ' '
 	echo '###########################################################################################################'
