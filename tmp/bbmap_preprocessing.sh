188df2d79643c47d5dd01c64eab437ee6ffac844
Kalyan Sahu <ksahu2@c0605.swan.hcc.unl.edu>
2024-05-21 15:31:26 -0500

    update prerpocessing

--- tmp/bbmap_preprocessing.sh
+++ tmp/bbmap_preprocessing.sh
@@ -1,4 +1,4 @@
 bbmap.sh minid=0.9 maxindel=3 bandwidthratio=0.16 bandwidth=12 quickmatch fast minhits=2 \
-    ref=/work/yaolab/fernandohq/metaIMP/bbmap/mousecatdoghuman/mm10.fa qtrim=r trimq=10 untrim usemodulo \
-    printunmappedcount kfilter=25 maxsites=1 k=14 bloom in1=$fastq1 \
-    in2=$fastq2  outu1=$clean1 outu2=$clean2
+	ref=/work/yaolab/fernandohq/metaIMP/bbmap/mousecatdoghuman/mm10.fa qtrim=r trimq=10 untrim usemodulo \
+    	printunmappedcount kfilter=25 maxsites=1 k=14 bloom in1=$fastq1 \
+    	in2=$fastq2  outu1=$clean1 outu2=$clean2
